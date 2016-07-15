52e9cd861ae17cc8034ac6012644d46d5d590d34
Bert McLees <bert.mclees@gmail.com>
2016-07-15 15:57:54 -0400

    Changed examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -7,11 +7,14 @@ var Examples = (props) => {
       <h1 className="text-center page-title">Examples</h1>
       <p>Here are a few example locations to try out:</p>
       <ol>
-        <li>
-          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+		<li>
+          <Link to='/?location=Dudley, GA'>Dudley, GA</Link>
         </li>
         <li>
-          <Link to='/?location=Rio'>Rio, Brazil</Link>
+          <Link to='/?location=Peachtree Corners, GA'>Peachtree Corners, GA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Martinez, GA'>Martinez, GA</Link>
         </li>
       </ol>
     </div>
